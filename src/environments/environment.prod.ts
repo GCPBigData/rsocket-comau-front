fd0f1e27aae2e6762cec5a3e4003416e99245bde
Jose RF Junior <web2ajax@gmail.com>
2020-07-08 12:22:30 -0400

    update componentes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  /*COMAU   φ = '1,61803398874988'; */
+
+  /*CIENTE*/
+  urlClienteLista: 'ws://localhost:8080/cliente',
+
+  /*CURSO*/
+  urlCursoLista: 'ws://localhost:8080/curso',
+
+  /*FORNECEDORES*/
+  urlFornecedoresLista: 'ws://localhost:8080/fornecedores',
+
+  /*PROJETOS*/
+  urlProjetosLista: 'ws://localhost:8080/projetos',
+
+  /*RECURSOS*/
+  urlRecursosLista: 'ws://localhost:8080/recursos',
+
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,21 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: true,
+  /*COMAU   φ = '1,61803398874988'; */
+
+  /*CIENTE*/
+  urlClienteLista: 'ws://localhost:8080/cliente',
+
+  /*CURSO*/
+  urlCursoLista: 'ws://localhost:8080/curso',
+
+  /*FORNECEDORES*/
+  urlFornecedoresLista: 'ws://localhost:8080/fornecedores',
+
+  /*PROJETOS*/
+  urlProjetosLista: 'ws://localhost:8080/projetos',
+
+  /*RECURSOS*/
+  urlRecursosLista: 'ws://localhost:8080/recursos',
+
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
