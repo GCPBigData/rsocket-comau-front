fb66738047aeb80e4537fdbf762e761563c0aa91
Jose RF Junior <web2ajax@gmail.com>
2020-07-08 21:42:33 -0400

    update componentes

--- src/app/clientesFisica/cliente-fisica-service.ts
+++ src/app/clientesFisica/cliente-fisica-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {ClienteFisica} from './cliente-fisica';
+import {ClienteFisica, RequestClienteFisica} from './cliente-fisica';
 import {environment} from '../../environments/environment';
 
 @Injectable({
@@ -9,13 +9,32 @@ import {environment} from '../../environments/environment';
 })
 export class ClienteFisicaService {
 
-  public readonly urlClienteLista = `${environment.urlClienteLista}`;
+  public readonly urlClienteFisicaLista    = `${environment.urlClienteFisicaLista}`;
+  public readonly ClienteFisicaURLfindNome = `${environment.ClienteFisicaURLfindNome}`;
+  public readonly ClienteFisicaURLfindId   = `${environment.ClienteFisicaURLfindId}`;
+  public readonly ClienteFisicaURLInsert   = `${environment.ClienteFisicaURLInsert}`;
+  public readonly ClienteFisicaURLPut      = `${environment.ClienteFisicaURLPut}`;
 
   constructor(private http: HttpClient) { }
 
   /*Metodo que OBSERVA o end point que lista os Clientes sem paginacao*/
   getClienteFisica(): Observable<ClienteFisica> {
-    return this.http.get<ClienteFisica>(this.urlClienteLista);
+    return this.http.get<ClienteFisica>(this.urlClienteFisicaLista);
   }
 
+  getId(id: string) {
+    return this.http.get(`${this.ClienteFisicaURLfindId}${id}`);
+  }
+
+  getNome(nome: string) {
+    return this.http.get(`${this.ClienteFisicaURLfindNome}${nome}`);
+  }
+
+  createClienteFisica(request: RequestClienteFisica): Observable<ClienteFisica> {
+    return this.http.post<ClienteFisica>(this.ClienteFisicaURLInsert, request);
+  }
+
+  updateClienteFisica(id: string, data: string) {
+    return this.http.put(`${this.ClienteFisicaURLPut}${id}`, data);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,20 +2,41 @@ export const environment = {
   production: true,
   /*COMAU   φ = '1,61803398874988'; */
 
-  /*CIENTE FISICA*/
-  urlClienteLista: '/clienteFisica/v1/todos',
+  /*CIENTE*/
+  urlClienteFisicaLista: 'http://localhost:8080/v1/cliente',
+  ClienteFisicaURLfindNome: 'http://localhost:8080/v1/cliente',
+  ClienteFisicaURLfindId: 'http://localhost:8080/v1/cliente',
+  ClienteFisicaURLInsert: 'http://localhost:8080/v1/cliente',
+  ClienteFisicaURLPut: 'http://localhost:8080/v1/cliente',
 
   /*CURSO*/
-  urlCursoLista: 'ws://localhost:8080/curso',
+  urlCursoLista: 'http://localhost:8080/v1/cliente',
+  cursoURLfindNome: 'http://localhost:8080/v1/cliente',
+  cursoURLfindId: 'http://localhost:8080/v1/cliente',
+  cursoURLInsert: 'http://localhost:8080/v1/cliente',
+  cursoURLPut: 'http://localhost:8080/v1/cliente',
 
   /*FORNECEDORES*/
   urlFornecedoresLista: 'ws://localhost:8080/fornecedores',
+  FornecedoresfindNome: 'http://localhost:8080/v1/cliente',
+  FornecedoresURLfindId: 'http://localhost:8080/v1/cliente',
+  FornecedoresURLInsert: 'http://localhost:8080/v1/cliente',
+  FornecedoresURLPut: 'http://localhost:8080/v1/cliente',
 
   /*PROJETOS*/
-  urlProjetosLista: 'ws://localhost:8080/projetos',
+  ProjetosLista: 'ws://localhost:8080/fornecedores',
+  ProjetosfindNome: 'http://localhost:8080/v1/cliente',
+  ProjetosURLfindId: 'http://localhost:8080/v1/cliente',
+  ProjetosURLInsert: 'http://localhost:8080/v1/cliente',
+  ProjetosURLPut: 'http://localhost:8080/v1/cliente',
 
   /*RECURSOS*/
   urlRecursosLista: 'ws://localhost:8080/recursos',
+  RecursosLista: 'ws://localhost:8080/fornecedores',
+  RecursosfindNome: 'http://localhost:8080/v1/cliente',
+  RecursosURLfindId: 'http://localhost:8080/v1/cliente',
+  RecursosURLInsert: 'http://localhost:8080/v1/cliente',
+  RecursosURLPut: 'http://localhost:8080/v1/cliente'
 
 };
 
